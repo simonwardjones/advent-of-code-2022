b4350e6819bc96a0e488f83dc99842890745ca5e
simonwardjones <simon.ward-jones@deliveroo.co.uk>
2022-12-06 11:13:56 +0000

    Day six only Python

---
+++ src/day_06/day_06.py
@@ -0,0 +1,35 @@
+import pathlib
+import re
+from queue import LifoQueue
+
+
+def load_data():
+    return pathlib.Path("inputs/day_06/data.txt").read_text()
+
+
+def part_one():
+    elf_data = load_data()
+    for letter_id, letter in enumerate(elf_data):
+        test = elf_data[letter_id : letter_id + 4]
+        if len(test) == len(set(test)):
+            print(letter_id + 4)
+            break
+
+
+def part_two():
+    elf_data = load_data()
+    print(elf_data)
+    for letter_id, letter in enumerate(elf_data):
+        test = elf_data[letter_id : letter_id + 14]
+        if len(test) == len(set(test)):
+            print(letter_id + 14)
+            break
+
+
+def main():
+    part_one()
+    part_two()
+
+
+if __name__ == "__main__":
+    main()
